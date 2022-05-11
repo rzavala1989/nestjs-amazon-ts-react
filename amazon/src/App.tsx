44519c9be231c2e38bf4e421733d1da0e8ad4fdd
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2022-05-10 23:18:51 -0700

    built auth section of FE

--- amazon/src/App.tsx
+++ amazon/src/App.tsx
@@ -1,12 +1,42 @@
-import { ThemeProvider } from '@emotion/react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+
 import { theme } from './shared/utils/theme';
+import HomePage from './pages/Home.page';
+import RegisterPage from './pages/Register.page';
+import SigninPage from './pages/Signin.page';
+import PrivateRoute from './features/auth/components/PrivateRoute';
+
+import { store } from './store';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      hi
+      <Router>
+        <Routes>
+          <Route path='/' element={<PrivateRoute page={<HomePage />} />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/signin' element={<SigninPage />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
+}
+
+type CypressWindow = Window & typeof globalThis & { Cypress: any; store: any };
+
+const thisWindow = window as CypressWindow;
+
+if (thisWindow.Cypress) {
+  console.log('CYPRESS WINDOW');
+
+  thisWindow.store = store;
 }
 
 export default App;
